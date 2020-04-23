38d971f304edc1b0a7a1803ef70f572c0d59988c
atul573 <atul573.sharma@gamil.com>
2020-04-23 12:37:37 +0530

    First update
    Signed-off-by: atul573 <atul573.sharma@gamil.com>

---
+++ lib/Note.dart
@@ -0,0 +1,74 @@
+class Note{
+int _id;
+String  _title;
+String _description;
+String _date;
+int _priority;
+
+Note(this._title,this._date,this._priority,[this._description]);
+Note.withId(this._id,this._title,this._date,this._priority,[this._description]);
+
+
+//all the getters
+int get id => _id;
+String get title => _title;
+String get description => _description;
+String get date => _date;
+int get priority => _priority;
+
+//all the setters
+
+set title( String newTitle){
+  if (newTitle.length <= 255) {
+    this._title = newTitle;
+    
+  }
+}
+set description( String newDescription){
+  if (newDescription.length <= 255) {
+    this._description = newDescription;
+    
+  }
+}
+set date( String newDate){
+  
+    this._date = newDate;
+    
+  
+}
+set priority( int newPriority){
+  if(newPriority >= 1 && newPriority <= 2){
+    
+    this._priority = newPriority;
+
+  }
+ 
+    
+  
+}
+
+//used to save and retrive from database
+
+//convert note object to map object
+Map<String, dynamic> toMap(){
+  var map = Map<String, dynamic>();
+  if(id != null){
+    map['id'] = _id;
+  }
+  map['title']= _title;
+  map['description']= _description;
+  map['priority'] = _priority;
+  map['date'] = _date;
+  return map;
+}
+
+Note.fromMaoObject(Map<String, dynamic> map){
+  this._id = map["id"];
+  this._title = map["title"];
+  this._description = map["description"];
+  this._priority = map["priority"];
+  this._date = map["date"];
+}
+
+
+}
